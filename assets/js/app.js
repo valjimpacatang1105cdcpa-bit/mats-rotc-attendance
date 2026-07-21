8e17497255cb6a25ddd6e800482230c1846de0d6
valjimpacatang1105cdcpa-bit <valjim.pacatang.1105cdcpa@gmail.com>
2026-07-21 21:05:34 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
+//-----------------------------------------
+// APP START
+//-----------------------------------------
 
+window.onload = function(){
+
+    startScanner();
+
+};
+
+//-----------------------------------------
+// STOP BUTTON
+//-----------------------------------------
+
+document.getElementById("stopBtn").addEventListener("click", async function(){
+
+    if(scanner){
+
+        try{
+
+            await scanner.stop();
+
+            setStatus("⏹ Scanner Stopped");
+
+        }catch(err){
+
+            console.log(err);
+
+        }
+
+    }
+
+});
