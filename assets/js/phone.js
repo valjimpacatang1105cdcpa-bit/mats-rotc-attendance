1b40f84ef667fb26e93983c36896deb8fdfe703b
valjimpacatang1105cdcpa-bit <valjim.pacatang.1105cdcpa@gmail.com>
2026-07-22 01:35:27 +0800

    Update phone.js

--- assets/js/phone.js
+++ assets/js/phone.js
@@ -1,1 +1,13 @@
+//-----------------------------------------
+// PHONE MODULE
+//-----------------------------------------
 
+if (isMobile) {
+
+    console.log("Phone Module Loaded");
+
+} else {
+
+    console.log("Desktop - Phone Module Disabled");
+
+}
