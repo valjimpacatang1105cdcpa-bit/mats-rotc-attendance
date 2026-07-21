6a2541617c6c236fa711e52496d511a47bd58de2
valjimpacatang1105cdcpa-bit <valjim.pacatang.1105cdcpa@gmail.com>
2026-07-21 20:59:19 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,6 @@
-// Config
+//-----------------------------------------
+// GOOGLE APPS SCRIPT API
+//-----------------------------------------
+
+const API_URL =
+"https://script.google.com/macros/s/AKfycbyjs55YWgb6owkyYckGoA_PlWS9VT0EdKvhjm8wEgZBcwCmfW_dsk4PwngTeurlFwBWVw/exec";
