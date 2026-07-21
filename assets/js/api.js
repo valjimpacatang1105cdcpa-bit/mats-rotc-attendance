d47602da37ceba8ad533b718f913a2e576b25673
valjimpacatang1105cdcpa-bit <valjim.pacatang.1105cdcpa@gmail.com>
2026-07-21 21:02:45 +0800

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,13 @@
+//-----------------------------------------
+// SEND QR TO GOOGLE APPS SCRIPT
+//-----------------------------------------
 
+async function sendAttendance(qr){
+
+    const response = await fetch(
+        API_URL + "?qr=" + encodeURIComponent(qr)
+    );
+
+    return await response.json();
+
+}
