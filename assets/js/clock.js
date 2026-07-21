f990aa201917db50218f83a7fe54114ba428dee5
valjimpacatang1105cdcpa-bit <valjim.pacatang.1105cdcpa@gmail.com>
2026-07-21 21:00:00 +0800

    Update clock.js

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,1 +1,19 @@
+function updateClock(){
 
+    const now = new Date();
+
+    document.getElementById("today").innerHTML =
+        now.toLocaleDateString("en-US",{
+            weekday:"long",
+            year:"numeric",
+            month:"long",
+            day:"numeric"
+        });
+
+    document.getElementById("clock").innerHTML =
+        now.toLocaleTimeString("en-US");
+
+}
+
+setInterval(updateClock,1000);
+updateClock();
