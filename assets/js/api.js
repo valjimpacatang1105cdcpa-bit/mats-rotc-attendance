b0f0132484b610216541aee639269de921a7e4fb
valjimpacatang1105cdcpa-bit <valjim.pacatang.1105cdcpa@gmail.com>
2026-07-22 17:52:39 +0800

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,10 +4,28 @@
 
 async function sendAttendance(qr){
 
-    const response = await fetch(
-        API_URL + "?qr=" + encodeURIComponent(qr)
-    );
+    try{
 
-    return await response.json();
+        console.log("Sending:", API_URL + "?qr=" + encodeURIComponent(qr));
+
+        const response = await fetch(
+            API_URL + "?qr=" + encodeURIComponent(qr)
+        );
+
+        console.log("STATUS:", response.status);
+
+        const text = await response.text();
+
+        console.log("RAW:", text);
+
+        return JSON.parse(text);
+
+    }catch(err){
+
+        console.log("FETCH ERROR:", err);
+
+        throw err;
+
+    }
 
 }
